29e344764f6da22cbaec1d8c72824e336c38e190
yangchenguang <yangchenguang@fpwis.com>
2024-02-22 14:59:49 +0800

    feat: 接口更换

--- src/api/module/user.tsx
+++ src/api/module/user.tsx
@@ -5,7 +5,7 @@
  * @Description: 用户API
  * @Date: 2024-02-02 14:28:52
  * @LastEditors: yangchenguang
- * @LastEditTime: 2024-02-18 15:22:52
+ * @LastEditTime: 2024-02-21 16:19:22
  */
 
 import serve, { Get, Post } from '@/api/index'
@@ -15,7 +15,7 @@ export const getUserInfo = (params: any) => {
 }
 
 export const testMyApi = (params: any) => {
-	return Get('http://localhost:3001/',params)
+	return Get('http://localhost:3001/getUserInfo',params)
 }
 
 export const testPostMyApi = (params: any) => {
